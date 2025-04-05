3bba4d4e8cbe53513577a964735e657b044b2a58
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-04-06 03:41:10 +0800

    update log files

--- job_bhargavi.sh
+++ job_bhargavi.sh
@@ -44,5 +44,5 @@ echo "Running script"
 # python /home/UG/bhargavi005/contrastive-emotion-recognition/src/train_and_test_bilstm/train_crowdflower.py
 # python /home/UG/bhargavi005/contrastive-emotion-recognition/src/preprocess/preprocess_ISEAR.py
 # python /home/UG/bhargavi005/contrastive-emotion-recognition/src/preprocess/preprocess_WASSA2021.py
-# python /home/UG/bhargavi005/contrastive-emotion-recognition/src/train_and_test_bilstm/train_isear.py
-python /home/UG/bhargavi005/contrastive-emotion-recognition/src/train_and_test_bilstm/train_wassa2021.py
+python /home/UG/bhargavi005/contrastive-emotion-recognition/src/train_and_test_bilstm/train_isear.py
+# python /home/UG/bhargavi005/contrastive-emotion-recognition/src/train_and_test_bilstm/train_wassa2021.py
